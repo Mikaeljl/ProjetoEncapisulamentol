28ea97d9b74763a8884e3db81a61f47f4df6a327
Mikael de Jesus <mikaeljl824@gmail.com>
2024-08-27 21:38:45 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoEncapisulamentol/ContaBancaria.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEncapisulamentol
+{
+    internal class ContaBancaria
+    {
+        private decimal Saldo;
+        public decimal saldo
+        {
+            get { return saldo; }
+            private set
+            {
+                if (value >= 0)
+                {
+                    saldo = value;
+                }
+            }
+
+        }
+
+        public ContaBancaria(decimal SaldoInicial)
+        { Saldo = SaldoInicial; }
+
+        public void Depositar(decimal quantia)
+        {
+            if (quantia > 0)
+            {
+                Console.WriteLine("Deposito realizado com sucesso.Saldo atualizado" + saldo);
+            }
+        }
+
+            public void Sacar(decimal quantia)
+            {
+                if (quantia > 0 && quantia <= Saldo)
+                        {
+                Saldo -= quantia;
+                Console.WriteLine("saque realizado com sucesso" + Saldo);
+
+            }
+
+            else
+            {
+                Console.WriteLine("erro ao sacar ");
+            }
+        }
+    }
+}
+
+
---
+++ ProjetoEncapisulamentol/Program.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoEncapisulamentol;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria(1000);
+        conta.Depositar(500);
+        conta.Sacar(200);
+        Console.ReadLine();
+
+    }
+
+}
